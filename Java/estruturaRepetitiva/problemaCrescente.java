5c4e583b085c8c2e014288d164f5c9d7c09c9694
daviaugustoo <davi.a.soares12@gmail.com>
2024-01-11 17:48:05 -0300

    Codigo java dos exercios de estrutura repetitiva

---
+++ Java/estruturaRepetitiva/problemaCrescente.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class problemaCrescente {
+    public static void main(String[] args) throws Exception {
+
+        int primeiroNumero, segundoNumero;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("Digite Dois números: ");
+            primeiroNumero = scanner.nextInt();
+            segundoNumero = scanner.nextInt();
+
+            String mensagem = (primeiroNumero == segundoNumero) ? "Iguais" : (primeiroNumero > segundoNumero) ? "Decrescente" : "Crescente";
+
+            System.out.println(mensagem);
+        }while(primeiroNumero != segundoNumero);
+
+        scanner.close();
+    }
+}
---
+++ Java/estruturaRepetitiva/problemaMediaIdades.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class problemaMediaIdades {
+    public static void main(String[] args) {
+        int idade = 0, idadeTotal = 0, totalPessoas = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite as idades:");
+        while (true) {
+            idade = scanner.nextInt();
+
+            if (idade <= 0) {
+                break;
+            }
+
+            idadeTotal += idade;
+            totalPessoas++;
+        }
+
+        if (totalPessoas == 0 || idadeTotal <= 0) {
+            System.out.println("ERRO.");
+        } else {
+            System.out.println("A MÉDIA É " + ((double) idadeTotal / totalPessoas));
+        }
+
+        scanner.close();
+    }
+}
