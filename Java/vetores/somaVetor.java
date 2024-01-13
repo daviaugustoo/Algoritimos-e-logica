d552e1ad4ae8f3255b4e859ea7e4341d1d7788f9
daviaugustoo <davi.a.soares12@gmail.com>
2024-01-12 21:32:36 -0300

    correçao do soma vetor

--- Java/vetores/somaVetor.java
+++ Java/vetores/somaVetor.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 public class somaVetor {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            int N, i;
 
-        int N, i;
+            System.out.print("Quantos numeros voce vai digitar? ");
+            N = sc.nextInt();
 
-        System.out.print("Quantos numeros voce vai digitar? ");
-        N = sc.nextInt();
+            double[] vet = new double[N];
+            double soma = 0;
 
-        double[] vet = new double[N];
-        double soma = 0;
+            for (i = 0; i < N; i++) {
+                System.out.print("Digite um numero: ");
+                vet[i] = sc.nextDouble();
+                soma += vet[i];
+            }
 
-        for (i = 0; i < N; i++) {
-            System.out.print("Digite um numero: ");
-            vet[i] = sc.nextDouble();
-            soma += vet[i];
-        }
+            System.out.print("\nVALORES: ");
+            for (i = 0; i < N; i++) {
+                System.out.print(String.format("%.1f ", vet[i]));
+            }
 
-        System.out.print("\nVALORES: ");
-        for (i = 0; i < N; i++) {
-            System.out.print(String.format("%.1f ", vet[i]));
-        }
+            System.out.print("\nSOMA: " + soma);
 
-        System.out.print("\nSOMA: " + soma);
+            System.out.print("\nMEDIA: " + (soma / N));
 
-        System.out.print("\nMEDIA: " + (soma / N));
-
-        sc.close(); 
+            sc.close();
+        } 
 
     }
 }
